dd5a3590beab4d67f27a13c8f0736a4d816185c3
YOUR_NAME <YOUR_EMAIL>
2023-08-08 12:16:06 +0300

    0.8_commit

--- crm/views.py
+++ crm/views.py
@@ -3,6 +3,7 @@ from .models import Order
 from .forms import OrderForm
 from cms.models import CmsSlider
 from price.models import PriceCard, PriceTable
+from telebot.sendmessage import sendTelegram
 # Create your views here.
 def first_page(request):
     slider_list = CmsSlider.objects.all()
@@ -20,8 +21,12 @@ def first_page(request):
                                             }
     return render(request, './index.html', dict_obj)
 def thanks_page(request):
-    name = request.POST['name']
-    phone = request.POST['phone']
-    element = Order(order_name = name, order_phone= phone)
-    element.save()
-    return render(request, './thanks_page.html', { 'name': name})
+    if request.POST:
+        name = request.POST['name']
+        phone = request.POST['phone']
+        element = Order(order_name=name, order_phone=phone)
+        element.save()
+        sendTelegram(tg_name=name, tg_phone=phone)
+        return render(request, './thanks_page.html', {'name': name})
+    else:
+        return render(request, './thanks_page.html')
--- thaimassagewebsite/urls.py
+++ thaimassagewebsite/urls.py
@@ -23,5 +23,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.first_page),
-    path('thanks/', views.thanks_page, name= 'thanks_page')
+    path('thanks/', views.thanks_page, name='thanks_page')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
